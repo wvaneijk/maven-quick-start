23e9acb32f776e96cf7d8db3068f0ce3dfa43bb9
William van Eijk <williamvaneijk@gmail.com>
2017-03-01 21:25:37 +0100

    Adding new dependencies

--- src/main/java/com/polteq/training/Application.java
+++ src/main/java/com/polteq/training/Application.java
@@ -2,9 +2,15 @@ package com.polteq;
 
 import java.util.ArrayList;
 import java.util.List;
+import org.apache.commons.lang3.StringUtils;
 
 public class Application {
     
+public int countWords(String words){
+	String[] seperateWords = StringUtils.split(words,' ');
+	return (seperateWords ==null)?0: seperateWords.length;
+}
+	
 	public void greet(){
 		List<String> greetings = new ArrayList<>();
 		greetings.add("Hello");
@@ -22,5 +28,7 @@ public class Application {
     	System.out.println ("Starting Application");
 	Application app = new Application();
 	app.greet();
+	int count = app.countWords("I have four words");
+	System.out.println("Word count:" +count);
     }
 }
